a77f3e940d0a2752fb7ebe7cb7ac62f559cadca5
flamolino <rafaelskate7@gmail.com>
2018-08-28 10:55:33 -0300

    +
    aula 28/08

--- app/src/main/java/br/unitins/cge/TelaPrincipal.java
+++ app/src/main/java/br/unitins/cge/TelaPrincipal.java
@@ -1,7 +1,7 @@
 package br.unitins.cge;
 
+import android.app.Activity;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
@@ -9,9 +9,7 @@ import android.util.Log;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import static java.lang.Thread.sleep;
-
-public class TelaPrincipal extends AppCompatActivity {
+public class TelaPrincipal extends Activity {
 
     //Declara uma referencia para a superficie de desenho;
     GLSurfaceView superficieDesenho = null;
@@ -37,55 +35,75 @@ public class TelaPrincipal extends AppCompatActivity {
         //publicar a superficie de desenho na tela
         setContentView(this.superficieDesenho);
 
-
-
-
-
     }
 
-
 }
+
+
 //Classe que ira implementar a logica do desenho (O que eu quero desenhar na superficie)
 class Renderizador implements Renderer{
 
-    private long cont = 0;
-    private boolean instancia = true;
+    private long contaQuadros = 0;
+    private long tempoInicial = 0;
+    private long tempoAtual = 0;
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        //Chamado quando a superficie de desenho é criada
+
+        tempoInicial = System.currentTimeMillis();
+        tempoAtual = System.currentTimeMillis();
+
+        //SETA A COR DE LIMPEZA DA TELA
+        gl.glClearColor(1, 0, 0, 1);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        //Chamado quandoa  superficie de desenho sofre alteração
-        Log.i("INFO", width+" "+height);
+
+        //configurando a area de coordenadas do plano cartesiano
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity(); //inicializa a matriz identidade
+
+
+        //configurando o volume de renderização
+        //coordenadas do universo
+        gl.glOrthox(0, width, 0, height, 1, -1);
+
+        //configurando a matriz de transf. geometricas
+        //translação, rotação e escala
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+
+        //configura a area de visualização na tela do dispositivo
+        //area da tela
+        gl.glViewport(0, 0, width, height);
+
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        //Metodo chamado para desenhar na telA
 
-        cont++;
+        float red, green, blue;
+        red = (float) Math.random();
+        green = (float) Math.random();
+        blue = (float) Math.random();
 
-        if(instancia) {
-            instancia = false;
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
+        tempoAtual = System.currentTimeMillis();
+        contaQuadros ++;
 
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    Log.i("FPS", cont + "");
-                    cont = 0;
-                    instancia = true;
-                }
-            }).start();
+        if(tempoAtual - tempoInicial >= 1000)
+        {
+            tempoInicial = System.currentTimeMillis();
+            Log.i("INFO", "FPS = " + contaQuadros);
+            contaQuadros = 0;
+            gl.glClearColor(red, green, blue, 1);
         }
+
+        //APLICA A COR DE LIMPEZA DE TELA A TODOS OS BITS DO BUFFER DE COR
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+
+
 
      }
 
